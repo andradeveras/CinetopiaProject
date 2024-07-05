6c7f5e8b0946a99cc95a0bf00cf7e3503d50e635
Leonardo Veras <andradeveras96@gmail.com>
2024-07-05 15:10:56 -0300

    Starting the project

---
+++ Cinetopia/Cinetopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinetopia
+//
+//  Created by Leonardo de Andrade Veras on 05/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
