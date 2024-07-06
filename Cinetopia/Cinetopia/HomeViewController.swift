3388d29d2b0d0603c75db52b9058dfae44c51335
Leonardo Veras <andradeveras96@gmail.com>
2024-07-06 17:39:53 -0300

    Organizing the constraints by functions

--- Cinetopia/Cinetopia/HomeViewController.swift
+++ Cinetopia/Cinetopia/HomeViewController.swift
@@ -11,18 +11,33 @@ class HomeViewController: UIViewController {
  
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .background
+        addSubViews()
+        setupConstraints()
         
-        let label = UILabel()
-        label.text = "Hello World"
-        label.textColor = .white
-        label.font = .systemFont(ofSize: 24, weight: .bold)
-        // Do any additional setup after loading the view.
-        
-        view.addSubview(label)
     }
 
-
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Filmes Populares"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        
+        return label
+    }()
+    
+    private func addSubViews(){
+        view.addSubview(label)
+    }
+    
+    private func setupConstraints(){
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32)
+        ])
+    }
 }
 
  
