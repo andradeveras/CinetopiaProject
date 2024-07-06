2ec60944877d20233f9fe559236dcc0ead5b32c8
Leonardo Veras <andradeveras96@gmail.com>
2024-07-06 19:10:00 -0300

    Building the home page

--- Cinetopia/Cinetopia/HomeViewController.swift
+++ Cinetopia/Cinetopia/HomeViewController.swift
@@ -17,26 +17,73 @@ class HomeViewController: UIViewController {
         setupConstraints()
         
     }
-
-    private lazy var label: UILabel = {
-        let label = UILabel()
+    
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage.logo)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    private lazy var coupleImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage.couple)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true //Mantem a qualidade da imagem em diferentes telas
+        
+        return imageView
+    }()
+    
+    private lazy var WelcomeLabel: UILabel = {
+       let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Filmes Populares"
-        label.textColor = .white
-        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.text = "O lugar ideal para buscar, salvar e organizar seus filmes favoritos!"
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .accent
+        label.textAlignment = .center
+        label.numberOfLines = 0
         
         return label
     }()
     
+    private lazy var welcomeButtom: UIButton = {
+        let buttom = UIButton()
+        buttom.translatesAutoresizingMaskIntoConstraints = false
+        buttom.setTitle("Quero começar!", for: .normal)
+        buttom.backgroundColor = .buttonBackground
+        buttom.setTitleColor(.background, for: .normal)
+        buttom.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        buttom.layer.cornerRadius = 32
+
+        return buttom
+    }()
+    
     private func addSubViews(){
-        view.addSubview(label)
+        view.addSubview(logoImageView)
+        view.addSubview(coupleImageView)
+        view.addSubview(WelcomeLabel)
+        view.addSubview(welcomeButtom)
     }
     
     private func setupConstraints(){
         NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
-            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32)
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            logoImageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            
+            coupleImageView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 32),
+            coupleImageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            
+            WelcomeLabel.topAnchor.constraint(equalTo: coupleImageView.bottomAnchor, constant: 32),
+            WelcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            WelcomeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+            WelcomeLabel.centerXAnchor.constraint(equalTo: coupleImageView.safeAreaLayoutGuide.centerXAnchor),
+            
+            welcomeButtom.topAnchor.constraint(equalTo: WelcomeLabel.bottomAnchor,constant: 32),
+            welcomeButtom.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 64),
+            welcomeButtom.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -64),
+            welcomeButtom.heightAnchor.constraint(equalToConstant: 64)
         ])
+        
+        
     }
 }
 
