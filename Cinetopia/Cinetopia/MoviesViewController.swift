94c742fe61aa44dc943be0b6681446aa19a1cd04
Leonardo Veras <andradeveras96@gmail.com>
2024-07-08 22:48:06 -0300

    add tableView - MoviesViewController

--- Cinetopia/Cinetopia/MoviesViewController.swift
+++ Cinetopia/Cinetopia/MoviesViewController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class MoviesViewController: UIViewController {
-
+    
+    private lazy var tableView: UITableView = {
+       let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .background
         setupNavigationBar()
+        addSubViews()
+        setupConstraints()
         
     }
     
@@ -24,6 +33,20 @@ class MoviesViewController: UIViewController {
         ]
         
         navigationItem.setHidesBackButton(true, animated: true)//remove back button
+    }
+    
+    private func addSubViews(){
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints(){
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            
+        ])
     }
     
 
